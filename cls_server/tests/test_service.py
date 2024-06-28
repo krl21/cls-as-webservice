29dddb338ce07ecb1f6c297857d3a0667718447f
Carlos Leon <carlos.leon@dox.cl>
2024-06-28 13:28:14 -0400

    rename: nombre de los tests

--- cls_server/tests/test_service.py
+++ cls_server/tests/test_service.py
@@ -5,7 +5,7 @@ import json
 
 class NumberClassifierTestCase(TestCase):
     
-    def test_list_cls(self):
+    def test_list_models_successful_response_and_structure(self):
         """
         Tests that the `/api/number-classifier/list_models/` endpoint returns a successful response (200 status code) and validates the expected structure of the response data:
         - Presence of 'success' and 'result' keys in the top-level response.
@@ -29,35 +29,25 @@ class NumberClassifierTestCase(TestCase):
             models_name = data['models']
             self.assertIsInstance(models_name, list, "The response should contain a models name list")
             
-    def test_predict(self):
-        """
-        Tests the 'predict_data' function for various scenarios:
+    def test_predict_invalid_json_request(self):
 
-        - Invalid JSON request (not valid JSON format).
-        - Missing 'values' key in the request body.
-        - Request body with 'values' key containing non-integer values.
-        - Successful prediction with a list of integers in the 'values' key (no model name specified).
-        - Successful prediction with a list of integers in the 'values' key and a recognized model name.
-        - Successful prediction with a list of integers in the 'values' key and an unrecognized model name.
-        """
-        
         client = Client()
         
-        ############
-        # Invalid JSON request
-        ############
         response = client.post('http://127.0.0.1:8000/api/number-classifier/predict/', data='not json"}', content_type='application/json')
+        
         self.assertEqual(response.status_code, 400)
         self.assertIn('Request body does not contain valid JSON.', response.json().get('error'))
         
-        ###############
-        # Missing 'values' key
-        ############
+    def test_predict_missing_values_key(self):
+        
+        client = Client()
+        
         response = client.post(
             'http://127.0.0.1:8000/api/number-classifier/predict/', 
-            json={}, 
+            data={}, 
             content_type='application/json'
         )
+        
         self.assertEqual(response.status_code, 200)
         
         response_data = json.loads(response.content)
@@ -74,13 +64,14 @@ class NumberClassifierTestCase(TestCase):
         error_msg = result['error_msg']
         self.assertIn("The 'values' key missing", error_msg)
         
-        ############
-        # Non-integer values
-        ############
+    def test_predict_non_integer_values(self):
+        
+        client = Client()
+        
         invalid_data = {'values': ['a', 'b', 'c']}
         response = client.post(
             'http://127.0.0.1:8000/api/number-classifier/predict/', 
-            json=invalid_data, 
+            data=invalid_data, 
             content_type='application/json'
         )
         
@@ -88,9 +79,10 @@ class NumberClassifierTestCase(TestCase):
         self.assertEqual(False, response.json().get('success', ''), "The value of the 'success' key should be False")
         self.assertIn("The 'values' elements must be integers", response.json().get('result', {}).get('error_msg'))
         
-        ############
-        # Test successful prediction
-        ############
+    def test_predict_successful_classification_no_model_specified(self):
+    
+        client = Client()
+        
         valid_data = {
             'values': [0, 1, 2, 3, 4, 5]
         }
@@ -110,36 +102,63 @@ class NumberClassifierTestCase(TestCase):
         
         response = client.post(
             'http://127.0.0.1:8000/api/number-classifier/predict/', 
-            json=valid_data, 
+            data=valid_data, 
             content_type='application/json'
         )
+        
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(True, response.json().get('success', ''), "The value of the 'success' key should be True")
         self.assertEqual(response.json(), result_data, 'Classification fails without specifying the model')
-
-        ############
-        # Valid request with model name (recognized)
-        ############
-        valid_data['model_name'] = 'decision_tree'
+        
+    def test_predict_successful_classification_with_recognized_model(self):
+        
+        client = Client()
+        
+        valid_data = {
+            'values': [0, 1, 2, 3, 4, 5],
+            'model_name': 'decision_tree'
+        }
+        result_data = {
+                'success': True,
+                'result': {
+                    'classification': [
+                        [0, "FizzBuzz"], 
+                        [1, "None"], 
+                        [2, "None"], 
+                        [3, "Fizz"], 
+                        [4, "None"], 
+                        [5, "Buzz"],
+                    ]
+                }
+            }
+        
         response = client.post(
             'http://127.0.0.1:8000/api/number-classifier/predict/', 
-            json=valid_data, 
+            data=valid_data, 
             content_type='application/json'
         )
+        
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(True, response.json().get('success', ''), "The value of the 'success' key should be True")
         self.assertEqual(response.json(), result_data, 'Classification fails with specifying the model')
-
-        ############
-        # Valid request with model name (unrecognized)
-        ############
-        valid_data['model_name'] = 'unknown_model'
+        
+    def test_predict_unrecognized_model_name(self):
+        
+        client = Client()
+        
+        valid_data = {
+            'values': [0, 1, 2, 3, 4, 5],
+            'model_name': 'unknown_model'
+        }
+        
         response = client.post(
             'http://127.0.0.1:8000/api/number-classifier/predict/', 
-            json=valid_data, 
+            data=valid_data, 
             content_type='application/json'
         )
+        
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(False, response.json().get('success', ''), "The value of the 'success' key should be False")
         self.assertIn("Model name is not recognized", response.json().get('result', {}).get('error_msg'))
         
-        
-    
-    
+ 
